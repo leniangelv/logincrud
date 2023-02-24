641b23523303298c1458365fa6be1c542f8e6125
Leniangel Pérez <102707929+leniangelv@users.noreply.github.com>
2023-02-24 19:57:34 -0400

    Update conexion.php

--- view/user/conexion.php
+++ view/user/conexion.php
@@ -1,15 +1,14 @@
 <?php
-function conectar(){
-    $host="localhost";
-    $user="root";
-    $pass="";
-
-    $bd="proyecto";
-
-    $con=mysqli_connect($host,$user,$pass);
-
-    mysqli_select_db($con,$bd);
-
-    return $con;
-}
+    session_start(); #Inicia una sesion o reanuda una exixtente
+    $servername = "localhost";   #Localhost o IP
+    $username = "root";          #Usuario de la dB
+    $password = "";              #Contraseña de la dB
+    $database = "proyecto";      #Nombre de la db
+    $port = "3306";              #puerto por el que se conecta la dB
+    $conn = mysqli_connect($servername, $username, $password, $database, $port);
+        if (!$conn) {
+        die("Conexion no establecida: " . mysqli_connect_error());
+        }
+        #mysqli_connect_error()
+        #devuelve una cadena con la descripcion del ultimo error de conexión
 ?>
