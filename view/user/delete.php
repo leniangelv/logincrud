4c9ea9e36aaa7bcf2988e728eeb29a570d7e3a8f
Leniangel Pérez <102707929+leniangelv@users.noreply.github.com>
2023-02-24 19:56:58 -0400

    Update delete.php

--- view/user/delete.php
+++ view/user/delete.php
@@ -1,14 +1,16 @@
 <?php
+    include ("conexion.php");
+    if (isset($_GET['cod_doctor'])){    
+        $cod_doctor = $_GET['cod_doctor'];
+        
+        $delete = "DELETE FROM doctor WHERE cod_doctor = $cod_doctor";
 
-include("conexion.php");
-$con=conectar();
-
-$cod_doctor=$_GET['id'];
-
-$sql="DELETE FROM doctor  WHERE cod_doctor='$cod_doctor'";
-$query=mysqli_query($con,$sql);
-
-    if($query){
-        Header("Location: index.php");
+        if (mysqli_query($conn, $delete)){
+            $_SESSION['message'] = 'Registro borrado exitosamente';
+            $_SESSION['message_type'] = 'danger'; # Funcion de bootstrap
+            header('Location:index.php'); # Redireccionar el archivo
+        }else{
+            echo "Error al borrar registro: " . mysqli_error($conn);
+        }
     }
 ?>
