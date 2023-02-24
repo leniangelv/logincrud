f4fa888e1f3246e373634616fc7c8251ae449abf
Leniangel Pérez <102707929+leniangelv@users.noreply.github.com>
2023-02-24 19:56:23 -0400

    Update index.php

--- view/user/index.php
+++ view/user/index.php
@@ -1,76 +1,129 @@
-<?php 
-    include("conexion.php");
-    $con=conectar();
-
-    $sql="SELECT *  FROM doctor";
-    $query=mysqli_query($con,$sql);
-
-    $row=mysqli_fetch_array($query);
-
-    echo '<a href="../../controller/cerrarSesion.php">LogOut</a>'
-?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Inicio</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    </head>
-    <body>
 
-            <div class="container mt-5">
-                    <div class="row"> 
-                        
-                        <div class="col-md-3">
-                            <h1>Ingrese datos</h1>
-                                <form action="insertar.php" method="POST">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Listado CMEG</title>
+    <!-- CDN de Bootstrap 4-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--Fontasome-->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css">
+    <script>
+        function validarform() {
+            var x = document.forms["form"]["cod_doctor"].value;
+            if (x == "") {
+                alert("El campo de Numero de Inscripcion no puede ir vacio");
+                return false;
+            }
+        }
+    </script>
+</head>
 
-                                    <input type="text" class="form-control mb-3" name="cod_doctor" placeholder="Numero de CMEG">
-                                    <input type="text" class="form-control mb-3" name="nombre" placeholder="Nombre Completo">
-                                    <input type="text" class="form-control mb-3" name="ci" placeholder="Cedula de Identidad">
-                                    <input type="text" class="form-control mb-3" name="fn" placeholder="Fecha de Nacimiento">
-                                    <input type="text" class="form-control mb-3" name="estado" placeholder="Status">
-                                    
-                                    <input type="submit" class="btn btn-primary">
-                                </form>
-                        </div>
-
-                        <div class="col-md-8">
-                            <table class="table" >
-                                <thead class="table-success table-striped" >
-                                    <tr>
-                                        <th>N° CMEG</th>
-                                        <th>Nombre</th>
-                                        <th>C.I.</th>
-                                        <th>Fecha de Nacimiento</th>
-                                        <th>Status</th>
-                                        <th></th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                        <?php
-                                            while($row=mysqli_fetch_array($query)){
-                                        ?>
-                                            <tr>
-                                                <th><?php  echo $row['cod_doctor']?></th>
-                                                <th><?php  echo $row['nombre']?></th>
-                                                <th><?php  echo $row['ci']?></th>
-                                                <th><?php  echo $row['fn']?></th>
-                                                <th><?php  echo $row['estado']?></th>       
-                                                <th><a href="actualizar.php?id=<?php echo $row['cod_doctor'] ?>" class="btn btn-info">Editar</a></th>
-                                                <th><a href="delete.php?id=<?php echo $row['cod_doctor'] ?>" class="btn btn-danger">Eliminar</a></th>                                        
-                                            </tr>
-                                        <?php 
-                                            }
-                                        ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>  
+<body>
+    <?php
+        echo '<a class="logout" href="../../controller/cerrarSesion.php">Cerrar Sesion</a>'
+    ?>
+    <div class="container">
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container">
+                <a href="index.php" class="navbar-brand">Listado de Medicos Inscritos CMEG</a>
             </div>
-    </body>
+        </nav>
+        <div class="container p-4">
+            <?php include("conexion.php");
+                        if(isset($_SESSION['message'])){?>
+            <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
+                <?=  $_SESSION['message']?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <?php session_unset(); } #Libera todas las variables de sesión ?>
+            <div class="row">
+                <div class="col-md-4 mx-auto">
+                    <div class="card card-body">
+                        <form method="post" name="form" onsubmit="return validarform()" action="create.php">
+                            <div class="form-group">
+                                <input type="text" name="cod_doctor" class="form-control" placeholder="Ingresar Numero de Inscripcion"
+                                    autocomplete="off" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="nombre" class="form-control" placeholder="Ingresar Nombre"
+                                    autocomplete="off" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="ci" class="form-control" placeholder="Ingresar Cedula de Identidad"
+                                    autocomplete="off" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="fn" class="form-control" placeholder="Ingresar Fecha de Nacimiento"
+                                    autocomplete="off" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="estado" class="form-control" placeholder="Ingresar Estado de la Inscripcion"
+                                    autocomplete="off" required>
+                            </div>
+                            <input type="submit" class="btn btn-success btn-block" name="send" value="Agregar">
+                            <input type="reset" class="btn btn-secondary btn-block" value="Limpiar">
+                        </form>
+                    </div>
+                </div> <!--End col-md-4-->
+                <div class="col-md-8 mx-auto">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Numero de CMEG</th>
+                                <th>Nombre</th>
+                                <th>C.I.</th>
+                                <th>Fecha de Nacimiento</th>
+                                <th>Status</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                                        $query = "SELECT * FROM doctor";
+                                        $result = mysqli_query($conn, $query);
+                                        while($row = mysqli_fetch_array($result)){ 
+                                        #Obtiene una fila de resultados como un array asociativo, numérico, o ambos
+                            ?>
+                            <tr>
+                                <td><?php echo $row['cod_doctor'] ?></td>
+                                <td><?php echo $row['nombre'] ?></td>
+                                <td><?php echo $row['ci'] ?></td>
+                                <td><?php echo $row['fn'] ?></td>
+                                <td><?php echo $row['estado'] ?></td>
+                                <td>
+                                    <a href="update.php?cod_doctor=<?php echo $row['cod_doctor']?>" class="btn btn-secondary">
+                                        <i class="fa fa-edit"></i>
+                                    </a>
+                                    <a href="delete.php?cod_doctor=<?php echo $row['cod_doctor']?>" class="btn btn-danger">
+                                        <i class="fa fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div> <!--End col-md-8-->
+            </div> <!--End row-->
+        </div><!--End container p-4-->
+    </div><!--End container-->
+    <!--Scripts-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+</body>
+
 </html>
